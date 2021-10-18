0c959154b56c3ac00e7570de85b0ebfbf7fe245f
cyberdr0id <gopstrops@gmail.com>
2021-10-18 16:48:05 +0300

    Added semicolons to the end of queries

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,8 +1,8 @@
 -- inserting users
-INSERT INTO USERS(NAME, PASSWORD) VALUES ('user1', 'password1'), ('user2', 'password2')
+INSERT INTO USERS(NAME, PASSWORD) VALUES ('user1', 'password1'), ('user2', 'password2');
 
 -- inserting admin
-INSERT INTO USERS(NAME, PASSWORD, ISADMIN) VALUES ('admin', 'admin', TRUE)
+INSERT INTO USERS(NAME, PASSWORD, ISADMIN) VALUES ('admin', 'admin', TRUE);
 
 -- inserting candidates
 INSERT INTO CANDIDATES(NAME, SURNAME, CVOSFILEID)
@@ -11,8 +11,8 @@ VALUES ('can1', 'didate1', '1111'),
 		('can3', 'didate3', '1111'),
 		('can4', 'didate4', '1111'),
 		('can5', 'didate5', '1111'),
-		('can6', 'didate6', '1111'), 
-		('can7', 'didate7', '1111')
+		('can6', 'didate6', '1111'),
+		('can7', 'didate7', '1111');
 
 -- inserting users requests
 INSERT INTO REQUESTS(USERID, CANDIDATEID, STATUS) VALUES (1,2, 'Rejected'), (1,5 ,'Submitted'), (1,6,'Submitted'), (2,1,'Submitted'), (2,3, 'Rejected'), (2,4, 'Rejected'), (1,7, 'Submitted');
