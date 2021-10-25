34cf4f2f813ee8d4e744f5332536e6ba6d36615b
cyberdr0id <gopstrops@gmail.com>
2021-10-25 18:03:43 +0300

    Added controllers

---
+++ internal/controllers/controllers.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func SignUp(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func LogIn(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func SendCV(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetRequests(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func LoadCV(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func FilterCV(rw http.ResponseWriter, r *http.Request) {
+
+}
