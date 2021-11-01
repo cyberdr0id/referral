c6cc4e4f3271f68aeba027ef41ec48f5868f5907
cyberdr0id <gopstrops@gmail.com>
2021-11-01 15:55:15 +0300

    Added full initialization to main file

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,43 @@ import (
 	"log"
 
 	"github.com/cyberdr0id/referral/internal/handler"
+	"github.com/cyberdr0id/referral/internal/repository"
 	"github.com/cyberdr0id/referral/internal/server"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	srv := new(server.Server)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error while reading config: %s", err.Error())
+	}
 
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	config := repository.DatabaseConfig{
+		Host:         viper.GetString("db.host"),
+		User:         viper.GetString("db.user"),
+		Password:     viper.GetString("db.password"),
+		DatabaseName: viper.GetString("db.dbname"),
+		Port:         viper.GetString("db.port"),
+		SSLMode:      viper.GetString("db.sslmode"),
+	}
+
+	db, err := repository.NewConnection(config)
+	if err != nil {
+		log.Fatalf("error while trying to connect to database: %s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
+	_ = repo
+
+	srv := server.NewServer(viper.GetString("port"), handler.InitRoutes())
+
+	if err := srv.Run(); err != nil {
 		log.Fatalf("error while starting server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("docs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
