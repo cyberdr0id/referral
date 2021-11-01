ed4a04375aed9dbffec561eb30eecb017e040ec9
cyberdr0id <gopstrops@gmail.com>
2021-11-01 18:44:56 +0300

    Removed custom errors

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -3,18 +3,9 @@ package repository
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 
 	_ "github.com/lib/pq"
-)
-
-var (
-	// errSqlConnection presents an error when sql connection cannot be created.
-	errSQLConnection = errors.New("cannot create sql connection")
-
-	// errPingDatabase presents an error when connection is created but database cannot be pinged.
-	errPingDatabase = errors.New("connection is created but database cannot be pinged")
 )
 
 // DatabaseConfig represents a type that contains database configuration data.
@@ -34,11 +25,11 @@ func NewConnection(config DatabaseConfig) (*sql.DB, error) {
 
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
-		return nil, errSQLConnection
+		return nil, err
 	}
 
 	if err = db.Ping(); err != nil {
-		return nil, errPingDatabase
+		return nil, err
 	}
 	return db, nil
 }
