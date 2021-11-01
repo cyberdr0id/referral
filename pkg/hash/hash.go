45075ef2b128f29ffb829c4f4440c2dc159e8558
cyberdr0id <gopstrops@gmail.com>
2021-11-01 15:51:07 +0300

    Added comments to hash methods

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,12 +1,15 @@
+// Package hash contains function for password hashing and comparing.
 package hash
 
 import "golang.org/x/crypto/bcrypt"
 
+// HashPassword transform password to hash-string.
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
+// CheckPasswordHash comparing password with his hash.
 func CheckPassowrdHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
