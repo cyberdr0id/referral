2c1a50d9b442bbb0702b96ec589cff8336e1476d
cyberdr0id <gopstrops@gmail.com>
2021-11-04 19:57:13 +0300

    Updated repository structure

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,34 +7,28 @@ import (
 
 // AuthRepository presents methods for user authorization/registration.
 type AuthRepository interface {
-	CreateUser(name, password string) (int, error)
+	CreateUser(name, password string) (string, error)
 	GetUser(name, password string) (User, error)
 }
 
 // ReferralRepositpry presents methods for candidates manipulating.
 type ReferralRepository interface {
 	GetRequests(id int) ([]Request, error)
-	AddCandidate(name, surname string, fileID int) (int, error)
-	GetCVID(id int) (int, error)
-}
-
-// IRepository unites AuthRepository and ReferralRepository and presents common interface for database layer.
-type IRepository interface {
-	AuthRepository
-	ReferralRepository
+	AddCandidate(name, surname string, fileID int) (string, error)
+	GetCVID(id int) (string, error)
 }
 
 // Candidate presents model of a sent candidate.
 type Candidate struct {
-	ID         int    `json:"id"`
+	ID         string `json:"id"`
 	Name       string `json:"name"`
 	Surname    string `json:"surname"`
-	CVOSFileID int    `json:"cvosfileid"`
+	CVOSFileID string `json:"cvosfileid"`
 }
 
 // User presents model of user.
 type User struct {
-	ID       int       `json:"id"`
+	ID       string    `json:"id"`
 	Name     string    `json:"name"`
 	Password string    `json:"password"`
 	IsAdmin  bool      `json:"isadmin"`
@@ -44,9 +38,9 @@ type User struct {
 
 // Request presents model of request.
 type Request struct {
-	ID          int       `json:"id"`
-	UserID      int       `json:"userid"`
-	CandidateID int       `json:"candidateid"`
+	ID          string    `json:"id"`
+	UserID      string    `json:"userid"`
+	CandidateID string    `json:"candidateid"`
 	Status      string    `json:"status"`
 	Created     time.Time `json:"created"`
 	Updated     time.Time `json:"updated"`
@@ -55,6 +49,8 @@ type Request struct {
 // Repository type which presents connection between database and app logic.
 type Repository struct {
 	db *sql.DB
+	AuthRepository
+	ReferralRepository
 }
 
 func NewRepository(db *sql.DB) *Repository {
