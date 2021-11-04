04632b1c5820263624d78df82d151ffb8337b164
cyberdr0id <gopstrops@gmail.com>
2021-11-04 19:59:34 +0300

    Made routes initializing on server

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -1,20 +1,12 @@
 // Package handler responsible for rounting.
 package handler
 
-import (
-	"github.com/gorilla/mux"
-)
-
 // InitRoutes initialize all endpoints.
-func InitRoutes() *mux.Router {
-	r := mux.NewRouter()
+func (s *Server) InitRoutes() {
+	s.Router.HandleFunc("/auth/login", s.LogIn).Methods("POST")   // user authorization
+	s.Router.HandleFunc("/auth/signup", s.SignUp).Methods("POST") // user registration
 
-	r.HandleFunc("/auth/login", LogIn).Methods("POST")   // user authorization
-	r.HandleFunc("/auth/signup", SignUp).Methods("POST") // user registration
-
-	r.HandleFunc("/references", SendCandidate).Methods("POST") // sending candidate
-	r.HandleFunc("/references", GetRequests).Methods("GET")    // user request history
-	r.HandleFunc("/cvs/{id}", DownloadCV).Methods("GET")       // loading cv
-
-	return r
+	s.Router.HandleFunc("/references", s.SendCandidate).Methods("POST") // sending candidate
+	s.Router.HandleFunc("/references", s.GetRequests).Methods("GET")    // user request history
+	s.Router.HandleFunc("/cvs", s.DownloadCV).Methods("GET")            // loading cv
 }
