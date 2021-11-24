bf91be1166031c5bc9b431646bcc625bce845e38
cyberdr0id <gopstrops@gmail.com>
2021-11-24 14:29:56 +0300

    Created common error, changed functions structures

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,14 +2,17 @@ package service
 
 import "github.com/cyberdr0id/referral/internal/repository"
 
+// ErrInvalidParameter presetns an error when user input invalid parameter.
+var ErrInvalidParameter = "invalid parameter"
+
 type Auth interface {
-	LogIn(name, password string) (string, error)
+	LogIn(name, password string) (string, string, error)
 	SignUp(name, password string) (string, error)
 }
 
 type Referral interface {
-	GetRequests(id int) ([]repository.Request, error)
-	AddCandidate(name, surname string, fileID int) (string, error)
-	GetCVID(id int) (string, error)
+	GetRequests(id, t string) ([]repository.Request, error)
+	AddCandidate(name, surname, fileID string) (string, error)
+	GetCVID(id string) (string, error)
 	UpdateRequest(id, status string) error
 }
