88ec8c687efe78a0b7e87010357dc1fca0838bc1
cyberdr0id <gopstrops@gmail.com>
2021-11-24 14:27:23 +0300

    Continued handlers test implementing

--- internal/handler/handlers_test.go
+++ internal/handler/handlers_test.go
@@ -2,39 +2,41 @@ package handler
 
 import (
 	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	mock_service "github.com/cyberdr0id/referral/internal/service/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestServer_LogIn(t *testing.T) {
+type Request struct {
+	name     string
+	password string
+}
 
+func TestServer_SignUp(t *testing.T) {
 	testTable := []struct {
 		testName             string
-		inputUserLogin       string
-		inputUserPassword    string
-		inputBody            string
+		request              Request
+		requestBody          string
 		expectedStatusCode   int
 		expectedResponseBody string
 		mock                 func(s *mock_service.MockAuth, name, password string)
 	}{
 		{
-			testName:          "Success",
-			inputUserLogin:    "Alexander",
-			inputUserPassword: "password",
-			inputBody: `{
-				"name":"Alexander",
-				"password":"password"
-			}`,
-			expectedStatusCode: 200,
-			expectedResponseBody: `{
-				"id":"75"
-			}`,
+			testName: "Success",
+			request: Request{
+				name:     "Nameee",
+				password: "Password",
+			},
+			requestBody:          `{"name":"Nameee","password":"password"}`,
+			expectedStatusCode:   http.StatusOK,
+			expectedResponseBody: `{"id":"1"}`,
 			mock: func(s *mock_service.MockAuth, name, password string) {
-				s.EXPECT().LogIn(name, password).Return("75", nil)
+				s.EXPECT().SignUp(name, password).Return("1", nil)
 			},
 		},
 	}
@@ -45,38 +47,94 @@ func TestServer_LogIn(t *testing.T) {
 			defer ctrl.Finish()
 
 			auth := mock_service.NewMockAuth(ctrl)
-			tc.mock(auth, tc.inputUserLogin, tc.inputUserPassword)
+			tc.mock(auth, tc.request.name, tc.request.password)
 
 			s := Server{
 				Auth: auth,
 			}
 
 			r := mux.NewRouter()
-			r.HandleFunc("/user/login", s.LogIn).Methods("POST")
+			r.HandleFunc("/auth/signup", s.SignUp).Methods("POST")
 
 			w := httptest.NewRecorder()
-			req := httptest.NewRequest("POST", "/auth/login", bytes.NewBufferString(tc.inputBody))
+			req := httptest.NewRequest("POST", "/auth/signup", bytes.NewBufferString(tc.requestBody))
 
-			s.Router.ServeHTTP(w, req)
+			r.ServeHTTP(w, req)
 
-			assertStatusCode(t, w.Code, tc.expectedStatusCode)
-			assertBody(t, w.Body.String(), tc.expectedResponseBody)
+			assert.Equal(t, tc.expectedStatusCode, w.Code)
+			assert.Equal(t, tc.expectedResponseBody, w.Body.String())
 		})
 	}
 }
 
-func assertBody(t *testing.T, got, want string) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("got body \n %s \n want \n %s \n", got, want)
+func TestServer_LogIn(t *testing.T) {
+	testTable := []struct {
+		testName             string
+		request              Request
+		requestBody          string
+		expectedStatusCode   int
+		expectedResponseBody string
+		mock                 func(s *mock_service.MockAuth, name, password string)
+	}{
+		{
+			testName: "Success",
+			request: Request{
+				name:     "Alexander",
+				password: "password",
+			},
+			requestBody:          `{"name":"Alexander","password":"password"}`,
+			expectedStatusCode:   http.StatusOK,
+			expectedResponseBody: `{"accessToken":"token","refreshToken":"token"}`,
+			mock: func(s *mock_service.MockAuth, name, password string) {
+				s.EXPECT().LogIn(name, password).Return("token", "token", nil)
+			},
+		},
+		{
+			testName: "Failure: empty password",
+			request: Request{
+				name:     "Alexander",
+				password: "",
+			},
+			requestBody:          `{"name":"Alexander","password":""}`,
+			expectedStatusCode:   http.StatusBadRequest,
+			expectedResponseBody: "invalid parameter: password\n",
+			mock:                 func(s *mock_service.MockAuth, name, password string) {},
+		},
+		{
+			testName: "Failure: empty name",
+			request: Request{
+				name:     "",
+				password: "password",
+			},
+			requestBody:          `{"name":"","password":""}`,
+			expectedStatusCode:   http.StatusBadRequest,
+			expectedResponseBody: "invalid parameter: name\n",
+			mock:                 func(s *mock_service.MockAuth, name, password string) {},
+		},
 	}
-}
 
-func assertStatusCode(t *testing.T, got, want int) {
-	t.Helper()
+	for _, tc := range testTable {
+		t.Run(tc.testName, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	if got != want {
-		t.Errorf("got %d status code, want %d", got, want)
+			auth := mock_service.NewMockAuth(ctrl)
+			tc.mock(auth, tc.request.name, tc.request.password)
+
+			s := Server{
+				Auth: auth,
+			}
+
+			r := mux.NewRouter()
+			r.HandleFunc("/auth/login", s.LogIn).Methods("POST")
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/auth/login", bytes.NewBufferString(tc.requestBody))
+
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatusCode, w.Code)
+			assert.Equal(t, tc.expectedResponseBody, w.Body.String())
+		})
 	}
 }
