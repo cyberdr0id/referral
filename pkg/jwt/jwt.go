c51fd65113b8a1bbc4efdd8941c3a8a3ba8038ab
cyberdr0id <gopstrops@gmail.com>
2021-12-02 18:14:23 +0300

    Created type for token, fixed some issues

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -7,45 +7,49 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-var key = []byte("Str0ngP@$$w0rd?##")
-
 type Claims struct {
-	Username string `json:"username"`
+	IsAdmin bool `json:"isAdmin"`
 	jwt.StandardClaims
 }
 
-func GenerateToken(username string) (string, error) {
-	claims := &Claims{
-		Username: username,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
-		},
-	}
-
-	tokenString, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(key)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
+type TokenManager struct {
+	key        string
+	expiryTime int64
 }
 
-func ParseToken(_token string) (string, error) {
+func NewTokenManager(key string, expiryTime int) *TokenManager {
+	return &TokenManager{
+		key:        key,
+		expiryTime: time.Now().Add(time.Minute * time.Duration(expiryTime)).Unix(),
+	}
+}
+
+func (t *TokenManager) GenerateToken(userID string, isAdmin bool) (string, error) {
+	return jwt.NewWithClaims(jwt.SigningMethodHS256, &Claims{
+		IsAdmin: isAdmin,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: t.expiryTime,
+			Subject:   userID,
+		},
+	}).SignedString(t.key)
+}
+
+func (t *TokenManager) ParseToken(_token string) (*jwt.StandardClaims, error) {
 	token, err := jwt.ParseWithClaims(_token, &jwt.StandardClaims{}, func(token *jwt.Token) (i interface{}, err error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("invalid signing method")
 		}
 
-		return []byte(key), nil
+		return []byte(t.key), nil
 	})
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("cannot parse token: %w", err)
 	}
 
 	claims, ok := token.Claims.(*jwt.StandardClaims)
 	if !ok {
-		return "", fmt.Errorf("cannot get claims from token")
+		return nil, fmt.Errorf("cannot get claims from token")
 	}
 
-	return claims.Subject, nil
+	return claims, nil
 }
