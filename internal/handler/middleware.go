cd0e8c106b0dc81149f8b83f67b205429761edcc
cyberdr0id <gopstrops@gmail.com>
2021-12-09 14:58:28 +0300

    Fixed authorization middleware, added more checks

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -7,29 +7,45 @@ import (
 )
 
 const (
-	emptyHeaderMessage = "authorization header cannot be empty"
-	emptyTokenMessage  = "JWT token cannot be empty"
+	authHeaderMessage     = "authorization header required"
+	emptyTokenMessage     = "JWT token cannot be empty"
+	invalidSecurityScheme = "invalid security scheme"
+	invalidAuthHeaderKey  = "invalid authorization header value"
 
-	authHeader   = "Authorization"
-	bearerHeader = "Bearer "
+	authHeaderKey = "Authorization"
+	bearerScheme  = "Bearer"
 )
 
 // AuthorizationMiddleware checks if user is authorized.
 func (s *Server) AuthorizationMiddleware(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		header := r.Header.Get(authHeader)
-		if header == "" {
-			sendResponse(rw, ErrorResponse{Message: emptyHeaderMessage}, http.StatusUnauthorized)
+		headerValue := r.Header.Get(authHeaderKey)
+		if headerValue == "" {
+			sendResponse(rw, ErrorResponse{Message: authHeaderMessage}, http.StatusUnauthorized)
+			return
 		}
 
-		token := strings.Split(header, bearerHeader)[1]
+		parts := strings.Split(headerValue, " ")
+		if len(parts) != 2 {
+			sendResponse(rw, ErrorResponse{Message: invalidAuthHeaderKey}, http.StatusUnauthorized)
+			return
+		}
+
+		securityScheme := parts[0]
+		token := parts[1]
+		if securityScheme != bearerScheme {
+			sendResponse(rw, ErrorResponse{Message: invalidSecurityScheme}, http.StatusUnauthorized)
+			return
+		}
 		if token == "" {
 			sendResponse(rw, ErrorResponse{Message: emptyTokenMessage}, http.StatusUnauthorized)
+			return
 		}
 
 		claims, err := s.Auth.ParseToken(token)
 		if err != nil {
-			sendResponse(rw, ErrorResponse{Message: fmt.Errorf("cannot parse JWT token: %w", err).Error()}, http.StatusInternalServerError)
+			sendResponse(rw, ErrorResponse{Message: fmt.Errorf("cannot parse JWT token: %w", err).Error()}, http.StatusUnauthorized)
+			return
 		}
 
 		currentUserID = claims.Subject
