57bab059e40c2054ea895bd0a9cd61c401ea6cc8
cyberdr0id <gopstrops@gmail.com>
2021-12-10 16:08:36 +0300

    Created package for work with application context

---
+++ pkg/context/context.go
@@ -0,0 +1,18 @@
+package context
+
+import "context"
+
+type key int
+
+var id key
+
+// Set sets the value in application context.
+func Set(ctx context.Context, userID string) context.Context {
+	return context.WithValue(ctx, id, userID)
+}
+
+// Get gets the value by key from application context.
+func Get(ctx context.Context) (string, bool) {
+	val, ok := ctx.Value(id).(string)
+	return val, ok
+}
