c9ea661996357972f57e883bdcc3b6d0ec935b02
cyberdr0id <gopstrops@gmail.com>
2022-02-18 20:54:44 +0300

    Add file with all necessary structs

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Database struct {
+	Host         string `envconfig:"DB_HOST"`
+	User         string `envconfig:"DB_USER"`
+	Password     string `envconfig:"DB_PASSWORD"`
+	DatabaseName string `envconfig:"DB_NAME"`
+	Port         string `envconfig:"DB_PORT"`
+	SSLMode      string `envconfig:"DB_SSLMODE"`
+}
+
+type JWT struct {
+	Key        string `envconfig:"JWT_KEY"`
+	ExpiryTime string `envconfig:"JWT_EXPIRY_TIME"`
+}
+
+type AWS struct {
+	Bucket      string `envconfig:"AWS_BUCKET"`
+	Region      string `envconfig:"AWS_REGION"`
+	AccessKey   string `envconfig:"AWS_ACCESS_KEY"`
+	AccessKeyID string `envconfig:"AWS_ACCESS_KEY_ID"`
+}
+
+type Application struct {
+	Port string `envconfig:"APP_PORT"`
+}
+
+type MainConfig struct {
+	DB  *Database
+	JWT *JWT
+	AWS *AWS
+	App *Application
+}
+
+func Load() (MainConfig, error) {
+	var c MainConfig
+
+	err := envconfig.Process("", &c)
+	fmt.Println(c)
+	return c, err
+}
